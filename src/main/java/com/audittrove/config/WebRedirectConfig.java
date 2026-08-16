64bd2c1ec89941c71761e40d3b547ba5557b81bc
Mert <mert.karacamm@gmail.com>
2026-08-16 21:57:32 +0300

    backend hatası düzeltildi.

--- src/main/java/com/audittrove/config/WebRedirectConfig.java
+++ src/main/java/com/audittrove/config/WebRedirectConfig.java
@@ -1,17 +1,14 @@
 package com.audittrove.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Gizlilik politikasi: /privacy adresi statik privacy.html sayfasina yonlenir.
 @Configuration
-public class WebRedirectConfig {
-    @Bean
-    OpenAPI auditTroveOpenApi() {
-        return new OpenAPI().info(new Info()
-                .title("AuditTrove API")
-                .version("1.0.0")
-                .description("RAG destekli finansal doküman denetim servisi"));
+public class WebRedirectConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/privacy", "/privacy.html");
     }
 }
